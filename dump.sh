771ad2b945d9f5fe9566c246a9590e9ea0235932
Richard Kovacs <kovacsricsi@gmail.com>
2022-06-21 21:58:32 +0200

    Create dump.sh

---
+++ dump.sh
@@ -0,0 +1,1 @@
+docker exec mariadb.service sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD nextcloud' > /media/wd/data/backup/nextcloud/nextcloud_$(date "+%s").sql && touch /media/wd/data/backup/nextcloud.ok
