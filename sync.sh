86532515c03fd0ec3408ead9fa6ed68c97649fcc
Richard Kovacs <kovacsricsi@gmail.com>
2024-02-27 23:43:54 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-rsync -az --delete --backup-dir=_deleted --exclude backup/services/var/lib/jellyfin/config/data/transcodes /media/wd/data/backup /media/wd/data/nextcloud 14121@ch-s012.rsync.net: && touch /media/wd/data/backup/rsync.ok
-rsync -az --delete --backup-dir=_carawonga /home/carawonga/backup 14121@ch-s012.rsync.net:carawonga && touch /media/wd/data/backup/carawonga.ok
+rsync -az --delete --timeout=600 --backup-dir=_deleted --exclude backup/services/var/lib/jellyfin/config/data/transcodes /media/wd/data/backup /media/wd/data/nextcloud 14121@ch-s012.rsync.net: && touch /media/wd/data/backup/rsync.ok
+rsync -az --delete --timeout=600 --backup-dir=_carawonga /home/carawonga/backup 14121@ch-s012.rsync.net:carawonga && touch /media/wd/data/backup/carawonga.ok
