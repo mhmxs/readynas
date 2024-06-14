0835de40d01d6c1feb37625a2f0a0cbf24c06e39
Richard Kovacs <kovacsricsi@gmail.com>
2024-06-14 22:10:17 +0200

    Delete dump.sh

--- dump.sh
+++
@@ -1,1 +0,0 @@
-docker exec mariadb.service sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD nextcloud' > /media/wd/data/backup/nextcloud/nextcloud_$(date "+%s").sql && touch /media/wd/data/backup/nextcloud.ok 
