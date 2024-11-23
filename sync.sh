8ca8ca2ca0ae539a89d915262884fad78a2fe29c
Richard Kovacs <kovacsricsi@gmail.com>
2024-11-23 14:51:06 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 docker exec mariadb.service sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD nextcloud' > /media/wd/data/backup/nextcloud/nextcloud_$(date "+%s").sql && touch /media/wd/data/backup/nextcloud.ok
-rsync -az --min-size=1 --delete --timeout=600 --backup-dir=_deleted --exclude backup/services/var/lib/jellyfin/config/data/transcodes /media/wd/data/backup /media/wd/data/nextcloud 14121@ch-s012.rsync.net: && touch /media/wd/data/backup/rsync.ok
+rsync -az --min-size=1 --delete --timeout=600 --backup-dir=_deleted --exclude backup/services/var/lib/jellyfin /media/wd/data/backup /media/wd/data/nextcloud 14121@ch-s012.rsync.net: && touch /media/wd/data/backup/rsync.ok
 rsync -az --min-size=1 --delete --timeout=600 --backup-dir=_carawonga /home/carawonga/backup 14121@ch-s012.rsync.net:carawonga && touch /media/wd/data/backup/carawonga.ok
