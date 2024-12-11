62fd69de40cc4e1920dd4cf97f8f3eacdcef57bf
Richard Kovacs <kovacsricsi@gmail.com>
2024-12-11 20:51:58 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 docker exec mariadb.service sh -c 'mysqldump -uroot -p$MYSQL_ROOT_PASSWORD nextcloud' > /media/wd/data/backup/nextcloud/nextcloud_$(date "+%s").sql && touch /media/wd/data/backup/nextcloud.ok
 find /media/wd/data/backup/nextcloud/ -type f -name *.sql -mtime +30 -exec rm -f {} \;
-rsync -az --min-size=1 --delete --timeout=30 --exclude *.db pi@192.168.3.108:/etc/pihole/ /media/wd/data/backup/pihole && touch /media/wd/data/backup/pihole.ok
 rsync -az --min-size=1 --delete --timeout=1800 --backup-dir=_deleted --exclude backup/services/var/lib/jellyfin /media/wd/data/backup /media/wd/data/nextcloud 14121@ch-s012.rsync.net: && touch /media/wd/data/backup/rsync.ok
 rsync -az --min-size=1 --delete --timeout=1800 --backup-dir=_carawonga /home/carawonga/backup 14121@ch-s012.rsync.net:carawonga && touch /media/wd/data/backup/carawonga.ok
