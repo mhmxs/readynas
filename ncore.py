ec672c299b871b019e57c97f912bf3e1f2e25e69
Richard Kovacs <kovacsricsi@gmail.com>
2025-04-06 09:24:59 +0200

    Create ncore.py

---
+++ ncore.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+from urllib.request import urlopen
+from os.path import expanduser
+import xml.etree.ElementTree as et
+
+url = os.environ['U']
+loc = os.environ['L']
+db = os.environ['D']
+
+file = open(db, 'r')
+existing = file.readlines()
+file.close()
+
+resp = urlopen(url)
+feed = resp.read().decode('utf-8')
+resp.close()
+
+root = et.fromstring(feed)
+
+for item in root[0].findall('item'):
+    url = item.find('link').text
+    name = url.split('=')[-1]
+
+    if name + '\n' in existing:
+        continue
+
+    resp = urlopen(url)
+    torrent = resp.read()
+    resp.close()
+
+    file = open(loc + '/' + name + '.torrent', "wb")
+    file.write(torrent)
+    file.close()
+
+    file = open(db, 'a')
+    file.write(name + '\n')
+    file.close()
